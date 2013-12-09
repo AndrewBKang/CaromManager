bebabe57e47acabd2fca3596ddd53e9e8dd99173
Andrew Kang <andrew.b.kang@gmail.com>
2013-12-09 12:50:53 -0500

    change template names to about and news, adjust refactor css, add figaro

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ CustomerManager::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
-
+  root 'welcome#news'
+  get 'about' => 'welcome#about', as: :about
+  get 'news' => 'welcome#news', as: :news
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
