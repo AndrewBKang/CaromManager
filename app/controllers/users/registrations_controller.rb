72bc92cd8f69c141f11697c18bea322bfd7c8c93
Andrew Kang <andrew.b.kang@gmail.com>
2014-01-01 11:43:47 -0500

    devise set up for confirmable and adding custom user attributes

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,10 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  private
+
+    def sign_up_params
+      allow = [:email, :password, :password_confirmation]
+      params.require(resource_name).permit(allow)
+    end
+
+end
